2538f3ed871120a22cc422844328bd94ac97dcbd
Safaa El Khayari <safaa.dk.1999@gmail.com>
2021-08-19 16:58:28 +0100

    home app navbar changes

---
+++ src/pages/home/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div>
+            navBar component
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/pages/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+function home() {
+    return (
+        <div>
+            <h4>navBar</h4>
+            <NavBar />
+
+        </div>
+    )
+}
+
+export default home
+
