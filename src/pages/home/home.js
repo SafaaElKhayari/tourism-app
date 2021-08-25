55e93f491cc3a080a766629056d22976bcf18e57
Safaa El Khayari <safaa.dk.1999@gmail.com>
2021-08-25 19:04:00 +0100

    conflict resolved

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,9 @@
-<<<<<<< HEAD
-import React from 'react'
+
+import React, { Component } from 'react'
+import DescriptionSection from './descriptionSection'
+import Footer from '../../components/navigation/footer'
+import UpcomingTrips from './upcomingTrips'
+import "../../styles/home.css"
 import NavBar from './NavBar'
 import Header_home from './Header_home'
 import Popular_destination from './Popular_destination'
@@ -7,28 +11,13 @@ import Popular_destination from './Popular_destination'
 function home() {
     return (
         <div>
-            <NavBar />
-            <Header_home/>
+             <NavBar />
+             <Header_home/>
              <Popular_destination/>
-
+             <DescriptionSection/>
+             <UpcomingTrips/>
+                <Footer/>
         </div>
     )
-=======
-import React, { Component } from 'react'
-import DescriptionSection from './descriptionSection'
-import Footer from '../../components/navigation/footer'
-import UpcomingTrips from './upcomingTrips'
-import "../../styles/home.css"
 
-export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <DescriptionSection/>
-                <UpcomingTrips/>
-                <Footer/> 
-            </div>
-        )
-    }
->>>>>>> 1c6ef9b276930b2bc8a6b48346d037653755d12b
 }
